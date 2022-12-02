84667d0d528b242f4646f91b8005ff84da0ced05
kevinkorza <kevingkorza@gmail.com>
2022-12-02 11:44:33 -0500

    SearchBooks refactored

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -70,12 +70,12 @@ const SearchBooks = () => {
     }
 
     try {
-      const response = await saveBook(bookToSave, token);
-      await saveSelectedBook({...bookToSave})
+      //Mutation
+      await saveSelectedBook({
+        variables: { saveBook }
+      })
 
-      if (!response.ok) {
-        throw new Error('something went wrong!');
-      }
+    
 
       // if book successfully saves to user's account, save book id to state
       setSavedBookIds([...savedBookIds, bookToSave.bookId]);
